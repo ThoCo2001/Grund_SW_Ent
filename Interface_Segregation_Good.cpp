89ccc6d6950bf6665991a8e0d33f081012db39a7
ThoCo2001 <thomas.colditz@audi.de>
2025-07-30 08:39:33 +0200

    Update Interface_Segregation_Good.cpp

--- Interface_Segregation_Good.cpp
+++ Interface_Segregation_Good.cpp
@@ -1,1 +1,61 @@
+#include <iostream>
+using namespace std;
 
+//Here we used "Interface Segregation"
+//This principle aims at splitting a set of actions into smaller sets so that a Class executes ONLY the set of actions it requires.
+//Clients are no longer forced to depend on methods that they do not use
+
+class FeatheredCreature {	//instead of a bird class we are using extra classes for feathered creatures
+public:
+	virtual void molt() = 0;
+};
+
+class FlyingCreature {		//for flying creatures
+public:
+	virtual void fly() = 0;
+};
+
+class SwimmingCreature {		//and for swimming creatures
+public:
+	virtual void swim() = 0;
+};
+
+class Eagle : public FlyingCreature, FeatheredCreature{	//The eagle only uses what he needs to fulfill its role
+public:
+	string currentLocation;
+	int numberOfFeathers;
+
+	Eagle(int initialNumberOfFeathers){
+		numberOfFeathers = initialNumberOfFeathers;
+	}
+
+	void fly() override{
+		currentLocation = "in the air";
+	}
+
+	void molt() override{
+		numberOfFeathers -= 1;
+	}
+};
+
+class Penguin : public SwimmingCreature,FeatheredCreature {	//And now the same is true for the penguin.
+public:
+	string currentLocation;
+	int numberOfFeathers;
+
+	Penguin(int initialNumberOfFeathers){
+		numberOfFeathers = initialNumberOfFeathers;
+	}
+
+	void swim() override {
+		currentLocation = "in the water";
+	}
+
+	void molt() override {
+		numberOfFeathers -= 2;
+	}
+};
+
+int main() {
+	return 0;
+}
